bd4d8c84e7cda24d33bb27718c388a60fd6f9790
liuxuhui <mrliuxuhui@gmail.com>
2021-05-06 11:23:48 +0800

    feat: add SpringWebDependenceBuilder implementation

--- src/main/java/com/flyingwillow/utt/domain/Dependence.java
+++ src/main/java/com/flyingwillow/utt/domain/Dependence.java
@@ -1,7 +1,13 @@
 package com.flyingwillow.utt.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Dependence {
     private String groupId;
     private String artifactId;
     private String version;
+    private String scope;
 }
--- src/main/java/com/flyingwillow/utt/extension/dependence/SpringWebDependenceBuilder.java
+++ src/main/java/com/flyingwillow/utt/extension/dependence/SpringWebDependenceBuilder.java
@@ -1,12 +1,24 @@
 package com.flyingwillow.utt.extension.dependence;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsSchema;
 import com.flyingwillow.utt.domain.Dependence;
 import com.flyingwillow.utt.domain.ProjectInfo;
 import com.flyingwillow.utt.extensionpoint.dependence.DependenceBuilder;
+import com.intellij.openapi.diagnostic.Logger;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 public class SpringWebDependenceBuilder implements DependenceBuilder {
+
+    private static final Logger LOG = Logger.getInstance(SpringWebDependenceBuilder.class);
+    private static final String DEFAULT_CONFIG = "springWebDependencies.properties";
+    private Properties properties;
+
     @Override
     public boolean match(ProjectInfo projectInfo) {
         return false;
@@ -14,6 +26,17 @@ public class SpringWebDependenceBuilder implements DependenceBuilder {
 
     @Override
     public List<Dependence> getDependenceList() {
-        return null;
+        JavaPropsMapper mapper = new JavaPropsMapper();
+        JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, Dependence.class);
+        try {
+            List<Dependence> dependencies = mapper.readerFor(type)
+                    .with(JavaPropsSchema.emptySchema())
+                    .withRootName("dependence")
+                    .readValue(SpringWebDependenceBuilder.class.getClassLoader().getResourceAsStream(DEFAULT_CONFIG));
+            return dependencies;
+        } catch (IOException e) {
+            LOG.error("read file exception!", e);
+        }
+        return Collections.emptyList();
     }
 }
